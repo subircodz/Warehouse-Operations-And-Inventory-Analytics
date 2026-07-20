24a69dbbba8933776140d3de8a1d725a9df3a041
Subir Sutradhar <ssutradhar35@gmail.com>
2026-07-20 21:08:23 +0530

    test: initialize pytest structure for validation package

---
+++ src/03-python/tests/test_record_count.py
@@ -0,0 +1,10 @@
+"""
+This module is responsible for testing the count_records()
+function.
+
+Author: Subir Sutradhar
+Date: 20/07/2026
+"""
+
+def test_count_records_returns_total_number_of_rows():
+    return
---
+++ src/03-python/utils/file_loader.py
@@ -0,0 +1,6 @@
+def load_data(source):
+    pass
+
+
+def read_excel(source):
+    pass
