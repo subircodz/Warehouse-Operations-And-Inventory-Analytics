b15f765a87897a5a589ce1b6f69b02d4a1b8226b
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-13 00:23:17 +0530

    refactor: export visualization generator

--- src/03-python/visualization/__init__.py
+++ src/03-python/visualization/__init__.py
@@ -1,1 +1,5 @@
 """Matplotlib-based visualization package for the warehouse analytics project."""
+
+from visualization.matplotlib_visualizations import generate_visualizations
+
+__all__ = ["generate_visualizations"]
